709b9da8489dbe122802f3db3a7b276ec84a7f92
fedeolivera1 <fedeolivera1@gmail.com>
2017-05-31 20:21:40 -0300

    Merge branch 'master' into branch1-dsk
    # Conflicts:
    #	GestPedDsk/src/gpd/interfaces/usuario/IPersUsuario.java
    #	GestPedDsk/src/gpd/manager/usuario/ManagerUsuario.java
    #	GestPedDsk/src/gpd/persistencia/usuario/PersistenciaUsuario.java
    #	GestPedDsk/src/gpd/presentacion/formulario/FrmLogin.java
    #	GestPedDsk/src/gpd/presentacion/formulario/FrmPrincipal.java

--- GestPedDsk/src/gpd/interfaces/usuario/IPersUsuario.java
+++ GestPedDsk/src/gpd/interfaces/usuario/IPersUsuario.java
@@ -6,8 +6,8 @@ import gpd.exceptions.PersistenciaException;
 public interface IPersUsuario {
 
 	public UsuarioDsk obtenerUsuario(String nombreUsuario, String password) throws PersistenciaException;
-	public void guardarUsuario(UsuarioDsk usuario);
-	public UsuarioDsk modificarUsuario(UsuarioDsk usuario);
-	public UsuarioDsk eliminarUsuario(UsuarioDsk usuario);
+	public Integer guardarUsuario(UsuarioDsk usuario);
+	public Integer modificarUsuario(UsuarioDsk usuario);
+	public Integer eliminarUsuario(UsuarioDsk usuario);
 	
 }
